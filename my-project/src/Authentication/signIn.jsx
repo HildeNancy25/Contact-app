337f77f5fc5b923316e5e0b196718cc7bfa8b63a
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-19 14:41:55 +0200

    changes

--- my-project/src/Authentication/signIn.jsx
+++ my-project/src/Authentication/signIn.jsx
@@ -1,40 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignIn = () => {
 
-  
+  const [response, setResponse] = useState({
+    type: '',
+    content: ''
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [signIn, setSignIn] = useState({
+    email: '',
+    password: ''
+  });
+
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    signInFunction(signIn)
+      .then(response => {
+        console.log(response);
+        setResponse({
+          type: 'success',
+          content: 'You are Logged IN !'
+        });
+        localStorage.setItem('user', JSON.stringify(response.user));
+        localStorage.setItem('token', response.token);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+        setSignIn({
+          email: '',
+          password: ''
+        });
+        window.location.replace('/Home')
+      })
+      .catch(error => {
+        console.log(error.message);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+        setResponse({
+          type: '',
+          content: ''
+        });
+      })
+  }
+
+  const handleInput = (event) => {
+    event.preventDefault({
+      ...SignIn,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return (
-    
+
     <section className='flex flex-col justify-center items-center mt-32'>
       <div className='w-5/12 h-96 border-2 border-gray-400 flex flex-col items-center'>
         <div className='text-2xl mt-5'>
           <b>SIGN IN</b>
         </div>
-        <form className='flex flex-col gap-3 mt-5'>
-            <label>
-                Full names:
-                <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='text' name='fullName'
-                 placeholder='enter names'/>
-            </label>
-            <label>
-                Email:
-                <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='email' name='email'
-                 placeholder='enter email'/>
-            </label>
-            <div className='flex flex-row gap-5'>
-                <Link to='' className=' text-blue-400 underline' >Forgot password ?</Link>
-                <Link to='/SignUp' className=' text-blue-400 underline' >Don't have account ?</Link>
-            </div>
-            <div className='mt-3'>
-              <Link to='/'
+        <form onSubmit={handleSignIn} className='flex flex-col gap-3 mt-5'>
+          <label>
+            Full names:
+            <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='text' name='fullName'
+              placeholder='enter names' onChange={handleInput} />
+          </label>
+          <label>
+            Email:
+            <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='email' name='email'
+              placeholder='enter email' onChange={handleInput} />
+          </label>
+          <div className='flex flex-row gap-5'>
+            <Link to='' className=' text-blue-400 underline' >Forgot password ?</Link>
+            <Link to='/SignUp' className=' text-blue-400 underline' >Don't have account ?</Link>
+          </div>
+          <div className='mt-3'>
+            <Link to='/'
               className='bg-yellow-500 p-2 border rounded-2xl mt-4 w-20'
-              > SIGN IN </Link>
-            </div>
+            >  
+              {loading && '...'}
+              {!loading && 'SIGN IN'}
+            </Link>
+          </div>
         </form>
+        <ResponseMessage type={response.type} content={respond.content} />
       </div>
     </section>
-    
+
   )
 }
 
--- my-project/src/Authentication/signUp.jsx
+++ my-project/src/Authentication/signUp.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+
+    const [response, setResponse] = useState({
+        type: '',
+        content: ''
+    });
+
+    const [loading, setLoading] = useState(false)
+    const [signUp, setSignUp] =useState({
+        Names: '',
+        email: '',
+        password: '',
+        confirmPassword:''
+    });
+
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        setLoading(true);
+
+        SignUpFunction(signUp)
+        .then(response => {
+            setResponse({
+                type: 'success',
+                content: response.message
+            });
+            setTimeout(() => {
+                setLoading(false);
+            }, 2000)
+            
+            setSignUp({
+                Names: '',
+                email: '',
+                password: '',
+                confirmPassword:''
+            })
+            window.location.replace('/SignIn');
+        })
+        .catch(error =>{
+            console.log(error.message)
+
+            setTimeout(() => {
+                setLoading(false)
+            }, 2000);
+            setResponse({
+                type: 'error',
+                content: error.message
+            });
+        })
+    } 
+
+    const handleInput = (event) => {
+        setSignUp({
+            ...SignUp,
+            [event.target.name]: event.target.value
+        })
+    }
+
   return (
     <>
     <section className='flex flex-col justify-center items-center mt-32'>
@@ -9,35 +66,39 @@ const SignUp = () => {
             <div className='text-2xl'>
                 <b>SIGN UP</b>
             </div>
-            <form className='flex flex-col gap-3'>
+            <form onSubmit={handleSignUp} className='flex flex-col gap-3'>
                 <label>
                     Full names:
                     <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='text' name='fullName'
-                    placeholder='enter names'/>
+                    placeholder='enter names' onChange={handleInput}/>
                 </label>
                 <label>
                     Email:
                     <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='email' name='email'
-                    placeholder='enter email'/>
+                    placeholder='enter email' onChange={handleInput}/>
                 </label>
                 <label>
                     Password:
                     <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='password' name='email'
-                    placeholder='enter password'/>
+                    placeholder='enter password' onChange={handleInput}/>
                 </label>
                 <label>
                     Confirm password:
                     <input className='flex border bg-gray-100 border-slate-500 p-2 md:w-80 sm:w-72' type='password' name='email'
-                    placeholder='re-enter password'/>
+                    placeholder='re-enter password'onChange={handleInput}/>
                 </label>
                 <div >
                     <Link to ="/SignIn" className=' text-blue-400 underline' >Already have account ?</Link>
                 </div>
                 <button type='submit'
                 className='bg-yellow-500 p-2 border rounded-2xl mt-4 w-32'
-                > SIGN UP </button>
+                > 
+                    {loading && '...'}
+                    {!loading && 'SIGN UP'}
+                </button>
 
             </form>
+            <ResponseMessage type={response.type} content={response.content} />
         </div>
     </section>
     </>
